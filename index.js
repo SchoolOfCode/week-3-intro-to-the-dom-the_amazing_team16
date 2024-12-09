15e0da29aeb6ef1813254027c12d68bee9de58d1
“olivcat” <184215778+olivcat@users.noreply.github.com>
2024-12-09 14:24:53 +0000

    Ticket 2b

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ console.log("Hello");
 console.log(document.getElementById("title"));
 console.log(document.getElementsByClassName("content"));
 console.log(document.getElementsByTagName("li"));
-<<<<<<< HEAD
-let firstListItem = document.querySelector('#itemList li');
+let firstListItem = document.querySelector("#itemList li");
 console.log(firstListItem);
 let parentElement = firstListItem.parentNode;
 console.log(parentElement);
-=======
-console.log(document.querySelector(".highlight"));
->>>>>>> 15c8ffe754a38bc54e3ece083873a1d578a4963b
+
+let itemList = document.querySelector("#itemList ul");
+console.log(itemList);
+let childElement = itemList.childNodes;
+console.log(childElement);
+
+/* Ticket 2b: Child Nodes
+Instructions:
+Select the <ul> element with the id of "itemList".
+Use the childNodes property to get all its child nodes.
+Log the child nodes to the console.
+Expected Outcome:
+The console should display a collection (NodeList) of child nodes of the <ul> element, which includes the <li> elements as well as any whitespace or line breaks.*/
