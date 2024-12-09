f7c5857a6f630931fdcaefa06eda969d578659ed
swirly-way <s4m14z1l@gmail.com>
2024-12-09 14:17:49 +0000

    Ticket 2a

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ console.log("Hello");
 console.log(document.getElementById("title"));
 console.log(document.getElementsByClassName("content"));
 console.log(document.getElementsByTagName("li"));
+let firstListItem = document.querySelector('#itemList li');
+console.log(firstListItem);
+let parentElement = firstListItem.parentNode;
+console.log(parentElement);
