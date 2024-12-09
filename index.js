15c8ffe754a38bc54e3ece083873a1d578a4963b
“olivcat” <184215778+olivcat@users.noreply.github.com>
2024-12-09 12:27:08 +0000

    task 1d completed

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("Hello");
 console.log(document.getElementById("title"));
 console.log(document.getElementsByClassName("content"));
 console.log(document.getElementsByTagName("li"));
+console.log(document.querySelector(".highlight"));
